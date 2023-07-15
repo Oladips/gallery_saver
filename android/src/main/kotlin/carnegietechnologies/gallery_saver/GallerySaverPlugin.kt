a329933596edce767f96de8e158d58960d0c607a
Adedoyin Oladipupo <abiodundipupo@gmail.com>
2023-07-15 12:43:38 +0100

    dependency resolved

--- android/src/main/kotlin/carnegietechnologies/gallery_saver/GallerySaverPlugin.kt
+++ android/src/main/kotlin/carnegietechnologies/gallery_saver/GallerySaverPlugin.kt
@@ -7,9 +7,10 @@ import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
+// import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+// import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
 class GallerySaverPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
 
